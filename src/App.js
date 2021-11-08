f8ff7c0a975f982a6d7a782ba7a4ba514356f961
ligia.bozzi@gmail.com <ligia.bozzi@gmail.com>
2021-11-08 15:29:21 -0300

    creating posts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Navbar from "./components/Navbar";
 import Stories from "./components/stories/Stories";
+import Posts from './components/posts/Posts'
+
 function App() {
   return (
     <div class="root">
@@ -7,6 +9,7 @@ function App() {
       <div class="corpo">
         <div class = "esquerda">
          <Stories /> 
+         <Posts />
         </div>
 
       </div>
---
+++ src/components/posts/Posts.js
@@ -0,0 +1,27 @@
+import Post from './Post'
+
+export default function Posts() {
+    return (
+        <div class="posts">
+
+            <Post
+                imgUser="assets/img/meowed.svg"
+                nameUser="meowed"
+                imgContent="assets/img/gato-telefone.svg"
+                imgLike="assets/img/respondeai.svg"
+                nameLike="respondeai"
+                numLikes='101.523'
+            />
+
+            <Post
+                imgUser="assets/img/barked.svg"
+                nameUser="barked"
+                imgContent="assets/img/dog.svg"
+                imgLike="assets/img/adorable_animals.svg"
+                nameLike="adorable_animals"
+                numLikes='99.159'
+            />
+
+        </div>
+    )
+}
